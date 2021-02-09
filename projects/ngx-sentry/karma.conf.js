c4ff901c4962dcc90b43f207c873aa074a924935
rtrm <rtrm@hcuge.ch>
2021-02-09 11:28:38 +0100

    ci(test): fix units tests

--- projects/ngx-sentry/karma.conf.js
+++ projects/ngx-sentry/karma.conf.js
@@ -5,6 +5,13 @@ module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    browsers: ['ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
--- projects/ngx-sentry/src/lib/ngx-sentry.service.spec.ts
+++ projects/ngx-sentry/src/lib/ngx-sentry.service.spec.ts
@@ -1,12 +1,24 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 
+import { NgxSentryModule } from './ngx-sentry.module';
 import { NgxSentryService } from './ngx-sentry.service';
 
 describe('NgxSentryService', () => {
   let service: NgxSentryService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        RouterModule.forRoot([]),
+        NgxSentryModule.forRoot({
+          dsn: '',
+          release: '1.0.0',
+          environment: 'UNITS-TESTS',
+          tracingOrigins: ['*'],
+        })
+      ]
+    });
     service = TestBed.inject(NgxSentryService);
   });
 
