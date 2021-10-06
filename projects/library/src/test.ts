8dc38f6e5114c937c1ac9b488104192c614fa730
Badisi <badisi84@gmail.com>
2021-10-06 22:17:49 +0200

    fix(lint)

--- projects/library/src/test.ts
+++ projects/library/src/test.ts
@@ -10,8 +10,8 @@ import {
 } from '@angular/platform-browser-dynamic/testing';
 
 declare const require: {
-    context(path: string, deep?: boolean, filter?: RegExp): {
-        keys(): string[];
+    context: (path: string, deep?: boolean, filter?: RegExp) => {
+        keys: () => string[];
         <T>(id: string): T;
     };
 };
