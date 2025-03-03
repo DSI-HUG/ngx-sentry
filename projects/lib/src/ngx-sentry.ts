31891bb90ba1d722f377614340235c7cd5f0d3da
Badisi <badisi84@gmail.com>
2025-03-03 14:04:46 +0100

    chore(deps): upgrade dependencies

--- projects/lib/src/ngx-sentry.ts
+++ projects/lib/src/ngx-sentry.ts
@@ -2,7 +2,8 @@
 import { APP_INITIALIZER, ErrorHandler, Provider } from '@angular/core';
 import { Router } from '@angular/router';
 import {
-    BrowserOptions, browserTracingIntegration, createErrorHandler, ErrorHandlerOptions, getCurrentScope, init, TraceService, User
+    BrowserOptions, browserTracingIntegration, createErrorHandler, ErrorHandlerOptions, getCurrentScope,
+    init, replayIntegration, TraceService, User
 } from '@sentry/angular';
 
 export const NGX_SENTRY_PROVIDERS = (options?: ErrorHandlerOptions): Provider[] => [{
@@ -35,9 +36,29 @@ export const setSentryUser = (user: User | null): void => {
 export const initSentry = (options: SentryOptions): void => {
     const defaultOptions = {
         autoSessionTracking: true,
-        integrations: [browserTracingIntegration()],
+        integrations: [
+            // Registers and configures the Tracing integration, which automatically instruments your
+            // application to monitor its performance, including custom Angular routing instrumentation
+            browserTracingIntegration(),
+
+            // Registers the Replay integration, which automatically captures Session Replays
+            replayIntegration()
+        ],
+
+        // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+        // We recommend adjusting this value in production
+        tracesSampleRate: 0.2,
+
+        // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
         tracePropagationTargets: options.tracePropagationTargets ?? ['localhost', /^\//],
-        tracesSampleRate: 0.2
+
+        // This sets the sample rate to be 10%. You may want this to be 100% while
+        // in development and sample at a lower rate in production
+        replaysSessionSampleRate: 0.1,
+
+        // If the entire session is not sampled, use the below sample rate to sample
+        // sessions when an error occurs.
+        replaysOnErrorSampleRate: 1.0
     };
 
     // Initialize Sentry
